77dc549f474fc246e739bd0157cc186faf416bab
Delmerie <j.delmerie@live.fr>
2022-03-17 21:54:29 +0100

    Validation du panier

--- src/Formation.java
+++ src/Formation.java
@@ -16,14 +16,14 @@ public class Formation {
 		System.out.println("Bonjour et bienvenue dans l'application FullTraining");
 		HashMap<Integer, ArrayList<String>> training = List.training(); // récup la liste des formations
 		HashMap<Integer, Integer> cart = new HashMap<Integer, Integer>(); // panier de l'utilisateur (ID DE LA FORMATION
-															// / QUANTITY)
+		// / QUANTITY)
 
 		boolean displayChoice = false;
 		int choiceMenu;
 
 		do {
+			System.out.println(cart);
 			displayMenu(); // affiche le menu principal
-			System.out.println(cart);	
 			while (scanner.hasNextInt() == false) // ignore la saisie tant qu'il ne s'agit pas d'un entier
 				scanner.next();
 			choiceMenu = scanner.nextInt(); // récup le choix du menu
@@ -45,17 +45,41 @@ public class Formation {
 				delete(training, cart);
 				displayChoice = true;
 				break;
-			case 5: // sortir de l'application
+			case 5: // passer commande
+				if (!cart.isEmpty()) {
+					commande(training, cart);
+					displayChoice = false;
+					validation(cart, displayChoice);
+				} else {
+					System.out.println("Votre panier est vide pour l'instant.");
+					displayChoice = true;
+				}
+				break;
+			case 6: // sortir de l'application
 				System.out.println("Vous avez quitté l'application, à bientôt !");
 				displayChoice = false;
 				break;
 			default:
-				System.out.println("Mauvaise saisie, recommencez");
+				System.out.println("Mauvaise saisie, recommencez !");
 				displayChoice = true;
 			}
 		} while (displayChoice);
 		scanner.close();
-;
+	}
+
+	/**
+	 * Cette méthode permet d'afficher le menu à l'utilisateur
+	 */
+	public static void displayMenu() {
+		System.out.println();
+		System.out.println("Que souhaitez-vous faire ? [Saisir le chiffre correspondant]");
+		System.out.println("-------------------------------------------------------------");
+		System.out.println("1 - Afficher la liste des formations");
+		System.out.println("2 - Ajouter une formation au panier");
+		System.out.println("3 - Afficher votre panier");
+		System.out.println("4 - Supprimer une formation du panier");
+		System.out.println("5 - Passer commande");
+		System.out.println("6 - Quitter l'application");
 	}
 
 	/**
@@ -77,20 +101,6 @@ public class Formation {
 			System.out.println();
 		}
 		System.out.println(hr);
-	}
-
-	/**
-	 * Cette méthode permet d'afficher le menu à l'utilisateur
-	 */
-	public static void displayMenu() {
-		System.out.println();
-		System.out.println("Que souhaitez-vous faire ? [Saisir le chiffre correspondant]");
-		System.out.println("-------------------------------------------------------------");
-		System.out.println("1 - Afficher la liste des formations");
-		System.out.println("2 - Ajouter une formation au panier");
-		System.out.println("3 - Afficher votre panier");
-		System.out.println("4 - Supprimer une formation du panier");
-		System.out.println("5 - Quitter l'application");
 	}
 
 	/**
@@ -123,15 +133,17 @@ public class Formation {
 			System.out.println("Souhaitez-vous ajouter une autre formation ? [Oui/Non]");
 			str = scanner.next();
 		}
+		System.out.println();
 	}
 
 	/**
 	 * Permet de supprimer une formation du panier
-	 * @param training
-	 * @param cart
+	 * 
+	 * @param training - Liste des formations proposées
+	 * @param cart     - Panier à remplir (ID de la formation / Quantité)
 	 */
 	public static void delete(HashMap<Integer, ArrayList<String>> training, HashMap<Integer, Integer> cart) {
-		if (cart.size() > 0) {
+		if (!cart.isEmpty()) {
 			System.out.println("Quelle formation souhaitez-vous supprimer du panier ? [Saisir l'ID de la formation]");
 			displayCart(training, cart);
 			while (scanner.hasNextInt() == false) // ignore la saisie tant qu'il ne s'agit pas d'un entier
@@ -139,11 +151,11 @@ public class Formation {
 			int id = scanner.nextInt();// récup le choix de l'user
 			if (cart.get(id) != null) { // si formation existe
 				cart.put(id, cart.get(id) - 1); // supp une formation
-				if(cart.get(id) == 0) { //si qté est à 0
+				if (cart.get(id) == 0) { // si qté est à 0
 					cart.remove(id); // supp du panier
 				}
 				System.out.println("La formation a bien été supprimée.");
-				
+
 			} else {
 				System.out.println("Cette formation ne figure pas dans votre panier.");
 			}
@@ -155,8 +167,8 @@ public class Formation {
 	/**
 	 * Affiche le contenu du panier
 	 * 
-	 * @param training
-	 * @param cart
+	 * @param training - Liste des formations proposées
+	 * @param cart     - Panier à remplir (ID de la formation / Quantité)
 	 */
 	public static void displayCart(HashMap<Integer, ArrayList<String>> training, HashMap<Integer, Integer> cart) {
 
@@ -180,6 +192,47 @@ public class Formation {
 			System.out.println(hr);
 		} else {
 			System.out.println("Votre panier est vide pour l'instant.");
+		}
+	}
+
+	public static void commande(HashMap<Integer, ArrayList<String>> training, HashMap<Integer, Integer> cart) {
+		System.out.println();
+		System.out.println("****************PAGE COMMANADE**************** \n");
+
+		String format = "%-2s | %-15s | %-8s | %-35s |%-8s |%-8s";
+		String hr = "-------------------------------------------------------------------------------------------";
+		System.out.println(hr);
+		System.out.format(format, "ID", "COURS", "NB/JOURS", "DESCRIPTION", "PRIX", "QUANTITE");
+		System.out.println();
+		System.out.println(hr);
+		int total = 0;
+		for (HashMap.Entry<Integer, ArrayList<String>> tr : training.entrySet()) {
+			for (HashMap.Entry<Integer, Integer> c : cart.entrySet()) {
+				if (tr.getKey() == c.getKey()) {
+					System.out.format(format, tr.getKey(), tr.getValue().get(0), tr.getValue().get(1),
+							tr.getValue().get(2), tr.getValue().get(3), c.getValue());
+					System.out.println();
+					total += (Integer.parseInt(tr.getValue().get(3))) * c.getValue();
+				}
+			}
+		}
+		System.out.println(hr);
+		System.out.format("TOTAL DU PANI ER : " + total + " € \n");
+	}
+	
+	public static void validation(HashMap<Integer, Integer> cart, boolean displayChoice) {
+		String str;
+
+		do {
+			System.out.println("Souhaitez-vous valider votre panier ? [Oui/Non]");
+			str = scanner.next();
+		} while (!str.equalsIgnoreCase("oui") && !str.equalsIgnoreCase("non"));
+		if (str.equalsIgnoreCase("oui")) {
+			System.out.println("Votre commande a bien été validé.");
+			cart.clear();
+			displayChoice = true;
+		} else {
+			System.out.println("Opération annulée. Retour au menu principal");  
 		}
 	}
 
